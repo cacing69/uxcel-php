53c0c34b42052a58e4cf34c0aee4979f3b1bdd3a
cacing69 <cacingworm69@gmail.com>
2020-07-04 16:56:29 +0700

    first commit

---
+++ src/Uxcel/Factory.php
@@ -0,0 +1,69 @@
+<?php
+namespace Uxcel;
+
+class Factory {
+	public $source;
+	protected $target;
+	public $dir;
+	public $id;
+	private $ext;
+	private $doc;
+	private $pathInfo;
+
+	function __construct() {
+        $this->doc = new \DOMDocument();
+    }
+
+	public function setSource($source)
+	{
+		$this->pathInfo = pathinfo($source);
+		$this->source = $source;
+	}
+
+	public function setTarget($target)
+	{
+		$this->target = $target;
+	}
+
+
+	function unProtect(){
+		$zip = new \ZipArchive();
+
+		$dir = $this->target.DIRECTORY_SEPARATOR.$this->pathInfo["filename"].'-'.$this->id ;
+
+		if ($zip->open($this->source) === true) {
+			$zip->extractTo($dir);
+			$zip->close();
+		} else {
+			throw new \Exception("Uxcel : failed to unzip file", 1);
+		}
+
+		$sheetDir = implode(DIRECTORY_SEPARATOR, [$dir, 'xl', 'worksheets']);
+
+		foreach (glob($sheetDir . DIRECTORY_SEPARATOR . '*.xml') as $sheet) {
+			$this->doc->load($sheet);
+			$element = $this->doc->documentElement;
+
+			foreach ($element->getElementsByTagName('sheetProtection') as $child) {
+				$element->removeChild($child);
+			}
+
+			$this->doc->save($sheet);
+		}
+
+		$zip = new \ZipArchive();
+		
+		$zip->open($this->target.DIRECTORY_SEPARATOR.$this->pathInfo["filename"]."-".$this->id.".".$this->pathInfo["extension"], \ZIPARCHIVE::CREATE);
+
+		$files = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir), \RecursiveIteratorIterator::LEAVES_ONLY);
+
+		foreach ($files as $file) {
+			if (!$file->isDir()) {
+				$file_path = $file->getRealPath();
+				$zip->addFile($file_path, substr($file_path, strlen($dir) + 1));
+			}
+		}
+
+		$zip->close();
+	}
+}
